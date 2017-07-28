701864561414a3f2ded26511e373c16c24db475e
marcuspappik <marcus.pappik@student.hpi.uni-potsdam.de>
2017-07-28 11:24:39 +0200

    updated

--- generation/cbf.py
+++ generation/cbf.py
@@ -25,12 +25,14 @@ std_generators = [generate_bell, generate_funnel, generate_cylinder]
 def generate_pattern_data(length, avg_pattern_length, avg_amplitude, default_variance = 1, variance_pattern_length = 10, variance_amplitude = 2, generators = std_generators, include_negatives = True):
     data = np.random.normal(0, default_variance, length)
     current_start = random.randint(0, avg_pattern_length)
-    current_length = math.ceil(random.gauss(avg_pattern_length, variance_pattern_length))
+    current_length = current_length = max(1, math.ceil(random.gauss(avg_pattern_length, variance_pattern_length)))
     
     while current_start + current_length < length:
         generator = random.choice(generators)
         current_amplitude = random.gauss(avg_amplitude, variance_amplitude)
         
+        while current_length <= 0:
+            current_length = -(current_length-1)
         pattern = generator(current_length, current_amplitude, default_variance)
         
         if include_negatives and random.random() > 0.5:
@@ -39,7 +41,7 @@ def generate_pattern_data(length, avg_pattern_length, avg_amplitude, default_var
         data[current_start : current_start + current_length] = pattern
         
         current_start = current_start + current_length + random.randint(0, avg_pattern_length)
-        current_length = max(0, math.ceil(random.gauss(avg_pattern_length, variance_pattern_length)))
+        current_length = max(1, math.ceil(random.gauss(avg_pattern_length, variance_pattern_length)))
     
     return data
         
